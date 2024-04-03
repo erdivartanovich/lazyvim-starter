e5975a54e16f9501c81c453604b62f04cd84de90
erdivartanovich <erdivartanovich@gmail.com>
2024-04-04 02:09:32 +0800

    disable indentblankline and user classic cmdline

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,5 +3,6 @@ return {
   { "nvimdev/dashboard-nvim", enabled = false },
   { "nvim-lualine/lualine.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
   { "echasnovski/mini.indentscope", enabled = false },
 }
--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -2,20 +2,9 @@ return {
   "folke/noice.nvim",
   opts = {
     cmdline = {
+      view = "cmdline",
       format = {
         filter = { pattern = "^:%s*!", icon = "󰢚 > ", lang = "bash" },
-      },
-    },
-    views = {
-      cmdline_popup = {
-        border = {
-          style = "none",
-          padding = { 2, 3 },
-        },
-        filter_options = {},
-        win_options = {
-          winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
-        },
       },
     },
   },
